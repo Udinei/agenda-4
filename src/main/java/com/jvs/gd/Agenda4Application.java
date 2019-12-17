aee664a301a68d690bf7cbc85d94bdf5b49359f9
Udinei Silva <ukaliko@gmail.com>
2019-12-17 12:23:59 -0400

    Projeto agenda-4

---
+++ src/main/java/com/jvs/gd/Agenda4Application.java
@@ -0,0 +1,13 @@
+package com.jvs.gd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agenda4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agenda4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvs/gd/Agenda4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvs.gd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agenda4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
